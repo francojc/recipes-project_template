55e3118a1a34937abc94e6949f54b32ac9c6cbc7
Jerid Francom <francojc@wfu.edu>
2017-08-28 12:08:08 -0400

    Updated '_pipeline.R' to include the `pacman` package, and code to source the analysis files

--- _pipeline.R
+++ _pipeline.R
@@ -10,18 +10,33 @@
 
 # Global project options or packages
 
+# Load `pacman`. If not installed, install then load.
+if (!require(package, character.only = TRUE)) {
+  install.packages(pacman)
+  library(pacman, character.only = TRUE)
+}
+
 # RUN ---------------------------------------------------------------------
 
 # 1. Acquire data
 
+source("code/acquire_data.R")
+
 # 2. Curate data
+
+source("code/curate_data.R")
 
 # 3. Transform data
 
+source("code/transform_data.R")
+
 # 4. Analyze data
+
+source("code/analyze_data.R")
 
 # 5. Generate reports
 
+source("report/article/report.Rmd")
 
 # LOG ---------------------------------------------------------------------
 
